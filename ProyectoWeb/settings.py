9928eb3d07ec1c30aa08b663b38ca65ae641348c
Jonathan Canales <94133323+Aukan-Wenuy@users.noreply.github.com>
2022-06-16 17:08:54 -0500

    Update settings.py

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -139,6 +139,6 @@ EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER="ale.astudillo.1989@gmail.com"
-EMAIL_HOST_PASSWORD="probzoogbtshbnob"
+EMAIL_HOST_PASSWORD=""
 
 
