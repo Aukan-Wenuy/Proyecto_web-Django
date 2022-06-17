3251233fe3f7b5aba99fcccb037c42372c8e0c7d
Jonathan <ingcanales.a@gmail.com>
2022-06-17 08:34:49 -0500

    Agrego nuevo pass de correo

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -138,7 +138,7 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="ale.astudillo.1989@gmail.com"
-EMAIL_HOST_PASSWORD="probzoogbtshbnob"
+EMAIL_HOST_USER="jonathancanalesastudillo@gmail.com"
+EMAIL_HOST_PASSWORD="opfuewpmtjrenpqt"
 
 
