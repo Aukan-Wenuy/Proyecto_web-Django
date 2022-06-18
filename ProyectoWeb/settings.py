e1b59b5778d7a8cac771bb87a038e7b8900834f7
Jonathan <ingcanales.a@gmail.com>
2022-06-18 14:56:52 -0500

    Actualizacion de Email y password

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -138,8 +138,9 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="X@gmail.com"
-EMAIL_HOST_PASSWORD="X"
+EMAIL_HOST_USER="jonathancanalesastudillo@gmail.com"
+EMAIL_HOST_PASSWORD="opfuewpmtjrenpqt"
+
 
 
 
